edd96801600332e8f829941fd3f37a6dd3a6824a
fukuta-tatsuya-intec <fukuta_tatsuya@intec.co.jp>
2020-09-01 13:44:17 +0900

    test

--- build_deb_cloud.sh
+++ build_deb_cloud.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -x
-
 if [ $# != 2 ]; then
   echo "Invalid Argment."
   echo " You need to specify the version number. (like 0.0.1)"
@@ -12,7 +10,19 @@ fi
 version_no=$1
 architect_code=$2
 
-gbp buildpackage -p"$(pwd)"/gpg-passphrase.sh --git-pristine-tar-commit --git-export-dir=../build-area -S -sd
+cp -rf ../rdbox-middleware ../rdbox-middleware-deb
+cd ../rdbox-middleware-deb/ || exit
+git branch --delete dfsg_clean
+git branch dfsg_clean upstream
+git checkout master
+git tag -d upstream/"${version_no}"
+gbp import-orig --no-merge -u "${version_no}" --pristine-tar ../rdbox_"${version_no}".orig.tar.gz
+git checkout dfsg_clean
+git pull --no-edit . upstream
+git checkout master
+git pull --no-edit . dfsg_clean
+rm -rf ../build-area/
+
 if ! gbp buildpackage -p"$(pwd)"/gpg-passphrase.sh --git-pristine-tar-commit --git-export-dir=../build-area -S -sd;
 then
   echo "Retry Over."
@@ -20,6 +30,7 @@ then
 fi
 
 # need sudo
+cd ../rdbox-middleware/ || exit
 if [ "$architect_code" = "armhf" ]; then
   sudo OS=raspbian DIST=buster ARCH=armhf pbuilder --build ../build-area/rdbox_"${version_no}".dsc
 elif [ "$architect_code" = "amd64" ]; then
@@ -29,3 +40,5 @@ elif [ "$architect_code" = "arm64" ]; then
 else
   sudo OS=raspbian DIST=buster ARCH=armhf pbuilder --build ../build-area/rdbox_"${version_no}".dsc
 fi
+
+exit 0
--- build_deb_local.sh
+++ build_deb_local.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-if [ $# != 2 ]; then
+if [ $# != 3 ]; then
   echo "Invalid Argment."
   echo " You need to specify the version number. (like 0.0.1)"
   echo " You need to specify Architect. (like armhf, amd64)"
+  echo " You neet to specify git branch. (like master, feature/app, develop...)"
   exit 1
 fi
 
 version_no=$1
 architect_code=$2
+branch=$3
 
-cp -rf ../rdbox-middleware ../rdbox-middleware-deb
+git pull origin "${branch}"
+git checkout "${branch}"
+commit_id=$(git rev-parse HEAD)
+git archive --format=tar.gz --prefix=rdbox/ -o ../rdbox_"${version_no}".orig.tar.gz "${commit_id}"
+
 cd ../rdbox-middleware-deb/ || exit
 git branch --delete dfsg_clean
 git branch dfsg_clean upstream
@@ -22,23 +28,17 @@ git pull --no-edit . upstream
 git checkout master
 git pull --no-edit . dfsg_clean
 rm -rf ../build-area/
-
-if ! gbp buildpackage -p"$(pwd)"/gpg-passphrase.sh --git-pristine-tar-commit --git-export-dir=../build-area -S -sd;
+if ! gbp buildpackage --git-pristine-tar-commit --git-export-dir=../build-area -S -sd;
 then
   echo "Retry Over."
   exit 1
 fi
 
 # need sudo
-cd ../rdbox-middleware/ || exit
 if [ "$architect_code" = "armhf" ]; then
   sudo OS=raspbian DIST=buster ARCH=armhf pbuilder --build ../build-area/rdbox_"${version_no}".dsc
 elif [ "$architect_code" = "amd64" ]; then
   sudo OS=debian DIST=buster ARCH=amd64 pbuilder --build ../build-area/rdbox_"${version_no}".dsc
-elif [ "$architect_code" = "arm64" ]; then
-  sudo OS=raspbian DIST=buster ARCH=arm64 pbuilder --build ../build-area/rdbox_"${version_no}".dsc
 else
   sudo OS=raspbian DIST=buster ARCH=armhf pbuilder --build ../build-area/rdbox_"${version_no}".dsc
 fi
-
-exit 0
