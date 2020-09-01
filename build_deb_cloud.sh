7fc0c3cf9b58fe200df3ae09b147ffee53df201e
fukuta-tatsuya-intec <fukuta_tatsuya@intec.co.jp>
2020-09-01 13:02:07 +0900

    test

--- build_deb_cloud.sh
+++ build_deb_cloud.sh
@@ -10,6 +10,7 @@ fi
 version_no=$1
 architect_code=$2
 
+gbp buildpackage -p"$(pwd)"/gpg-passphrase.sh --git-pristine-tar-commit --git-export-dir=../build-area -S -sd
 if ! gbp buildpackage -p"$(pwd)"/gpg-passphrase.sh --git-pristine-tar-commit --git-export-dir=../build-area -S -sd;
 then
   echo "Retry Over."
